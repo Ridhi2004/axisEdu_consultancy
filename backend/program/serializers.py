81f9be11db01893cc740089007d6917adf90d155
Arjun Dhakal <arjundhakal8330@gmail.com>
2026-07-19 11:39:22 +0545

    registaration

--- backend/program/serializers.py
+++ backend/program/serializers.py
@@ -20,3 +20,42 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class ContactSerializer(serializers.Serializer):
+    full_name = serializers.CharField(max_length=500)
+    phone = serializers.CharField(max_length=20, required=False)
+    email = serializers.EmailField()
+    message = serializers.CharField()
+
+
+class RegistrationSerializer(serializers.Serializer):
+    course_country = serializers.CharField(max_length=255)
+    name = serializers.CharField(max_length=255)
+    gender = serializers.ChoiceField(choices=["male", "female"])
+    permanent_address = serializers.CharField()
+    temporary_address = serializers.CharField(required=False, allow_blank=True)
+    contact_number = serializers.CharField(max_length=20)
+    email = serializers.EmailField()
+    dob = serializers.DateField()
+    guardian = serializers.CharField(max_length=255)
+    occupation_income = serializers.CharField()
+    nationality = serializers.CharField(max_length=100)
+    language_known = serializers.CharField()
+    financial_sponsor_name = serializers.CharField(max_length=255)
+    sponsor_contact = serializers.CharField(max_length=20)
+    sponsor_address = serializers.CharField()
+    sponsor_email = serializers.EmailField()
+    academic_details = serializers.CharField()
+    ielts_toefl_score = serializers.CharField(
+        required=False,
+        allow_blank=True
+    )
+    course_willing_to_study = serializers.CharField(max_length=255)
+    hear_about = serializers.ChoiceField(
+        choices=[
+            "Advertisement",
+            "Friends",
+            "Internet"
+        ]
+    )
